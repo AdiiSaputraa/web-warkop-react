3cd24029dad410c5583c2e69ad57a11b7849cd3e
Adi Saputra <160607459+AdiiSaputraa@users.noreply.github.com>
2024-11-17 22:52:54 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
-import './dist/css/main.css';
+import './styles/main.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.jsx'
 import ScrollToTop from './components/ScrollToTop.jsx';
